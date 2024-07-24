9a44f1e85fa826059313f33370b85b07709eb138
Reid E <loess_sobs0n@icloud.com>
2024-07-23 20:31:28 -0400

    API returns first result. Added style sheet for results.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 //API
+const bookItem = [];
+const bookBtn = document.getElementById('bookSearchButton');
 async function pullBook(book) {
   const url = `https://www.loc.gov/search/?digitized=true&q=${book}&fo=json`;
   const res = await fetch(url);
@@ -6,6 +8,9 @@ async function pullBook(book) {
     throw new Error(`${res.status}`);
   }
   const data = await res.json();
-
-  console.log(data.results);
+  const bookImg = document.getElementById('bookImage');
+  bookImg.src = data.results[0].image_url[0];
+  const bookDescription = document.getElementById('bookResults');
+  bookDescription.innerHTML = `<h3>${data.results[0].title}</h3><p>${data.results[0].description}</p>`;
+  bookDescription.appendChild(bookImg);
 }
