3e67fa06c24ec11fb78cdf10c598cd0676c55ed8
Reid E <loess_sobs0n@icloud.com>
2024-07-30 19:23:23 -0400

    added some comments, continued work on api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-//API
+//API set up
 let bookList = [];
 const bookResults = document.getElementById('bookBox');
 const bookBtn = document.getElementById('bookSearchButton');
 let bookNum = 0;
-
+//API
 async function pullBook(book) {
   const rawRes = await fetch(
     `https://www.loc.gov/books/?digitized=true&q=${book}&fo=json&at=results`
@@ -15,6 +15,7 @@ async function pullBook(book) {
   console.log(res);
   bookList.push(...res.results);
   bookList.forEach(() => {
+    //create elements for cards and add classes
     let newBook = document.createElement('div');
     newBook.classList.add('card', 'bg-light', 'mb-3');
     let bookImg = document.createElement('img');
@@ -23,32 +24,43 @@ async function pullBook(book) {
     bookTitle.classList.add('card-title', 'ms-3');
     let bookDescription = document.createElement('p');
     bookDescription.classList.add('card-text', 'mx-5');
+    //check if LOC has img, if not add formatting class to placeholder
     if (res.results[bookNum].image_url[0] == undefined) {
       bookImg.src = '../imgs/book-app-icon.png';
       bookImg.classList.add('resultsIcon');
     } else {
       bookImg.src = res.results[bookNum].image_url[0];
     }
-    // bookImg.src = res.results[bookNum].image_url[0];
     bookTitle.innerHTML = `${res.results[bookNum].title} <br><small class="text-body-secondary">Call #:${res.results[bookNum].shelf_id}</small>`;
     bookDescription.innerHTML = `${res.results[bookNum].description} <a href="${res.results[bookNum].aka[0]}" target="_blank">Read More at LOC</a>`;
+    //build the card
     newBook.append(bookImg, bookTitle, bookDescription);
+    //add card to list
     bookResults.appendChild(newBook);
+    //increment to move to next result
     bookNum++;
   });
 }
-
+//Reset API function
+function resetBooks() {
+  const element = document.getElementById('bookBox');
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  bookList = [];
+  bookNum = 0;
+}
+//Form submit
 document.querySelector('#book-search-form').addEventListener('submit', (e) => {
   //block submit function
   e.preventDefault();
   //retrieve values
   let book = document.querySelector('#search-title').value;
 
-  //validate submission
+  //check for book
   if (book === '') {
     alert('What are you looking for?');
   } else {
-    bookList = [];
     pullBook(book);
     document.querySelector('#search-title').value = '';
   }
