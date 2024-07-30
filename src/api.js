78f2f4c11d1dab2fb086e1acca25bde61fb58ff9
Reid E <loess_sobs0n@icloud.com>
2024-07-30 08:52:37 -0400

    api page now showing results list, corrected broken image issue

--- src/api.js
+++ src/api.js
@@ -1,16 +1,39 @@
 //API
-const bookItem = [];
+const bookList = [];
+const bookResults = document.getElementById('bookBox');
 const bookBtn = document.getElementById('bookSearchButton');
+let bookNum = 0;
+
 async function pullBook(book) {
-  const url = `https://www.loc.gov/search/?digitized=true&q=${book}&fo=json`;
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error(`${res.status}`);
+  const rawRes = await fetch(
+    `https://www.loc.gov/books/?digitized=true&q=${book}&fo=json&at=results`
+  );
+  if (!rawRes.ok) {
+    throw new Error(`${rawRes.status}`);
   }
-  const data = await res.json();
-  const bookImg = document.getElementById('bookImage');
-  bookImg.src = data.results[0].image_url[0];
-  const bookDescription = document.getElementById('bookResults');
-  bookDescription.innerHTML = `<h3>${data.results[0].title}</h3><p>${data.results[0].description}</p>`;
-  bookDescription.appendChild(bookImg);
+  let res = await rawRes.json();
+  console.log(res);
+  bookList.push(...res.results);
+  bookList.forEach(() => {
+    let newBook = document.createElement('div');
+    newBook.classList.add('card', 'bg-light', 'mb-3');
+    let bookImg = document.createElement('img');
+    bookImg.classList.add('bookImg');
+    let bookTitle = document.createElement('h3');
+    bookTitle.classList.add('card-title', 'ms-3');
+    let bookDescription = document.createElement('p');
+    bookDescription.classList.add('card-text', 'mx-5');
+    if (res.results[bookNum].image_url[0] == undefined) {
+      bookImg.src = '../imgs/book-app-icon.png';
+      bookImg.classList.add('resultsIcon');
+    } else {
+      bookImg.src = res.results[bookNum].image_url[0];
+    }
+    // bookImg.src = res.results[bookNum].image_url[0];
+    bookTitle.innerHTML = `${res.results[bookNum].title} <br><small class="text-body-secondary">Call #:${res.results[bookNum].shelf_id}</small>`;
+    bookDescription.innerText = `${res.results[bookNum].description}`;
+    newBook.append(bookImg, bookTitle, bookDescription);
+    bookResults.appendChild(newBook);
+    bookNum++;
+  });
 }
