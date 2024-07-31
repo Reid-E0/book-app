5c1f1b3455e05b4aa647b50845d5c4d1c7a86632
Reid E <loess_sobs0n@icloud.com>
2024-07-31 16:31:17 -0400

    moved imgs into src, added homepage content and style, updated about me

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const favicon = require('serve-favicon');
 const path = require('path');
 const app = express();
 const port = 3000;
-app.use(favicon(path.join(__dirname, 'imgs', 'book-app-icon.png')));
-
+app.use(favicon(path.join(__dirname, 'src', 'book-app-icon.png')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/homepage.html'));
 });
@@ -18,7 +17,6 @@ app.get('/aboutme.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/aboutMe.html'));
 });
 app.use(express.static('src'));
-app.use(express.static('imgs'));
 app.listen(port, () => {
   console.log(`Listening ${port}`);
 });
